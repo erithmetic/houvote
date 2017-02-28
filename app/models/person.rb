fb42e231c37769df17d1462732f045a8f5eec66c
Derek Kastner <dkastner@gmail.com>
2017-02-27 21:42:49 -0600

    Make person photo field

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,6 @@ class Person < ApplicationRecord
   self.primary_key = :slug
   has_many :terms, foreign_key: :person_slug  # lol maybe not
   has_many :governemnts, through: :terms
+
+  mount_uploader :photo, PhotoUploader
 end
