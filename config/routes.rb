c66dbaf4bdd8a9363dee45d198787c4143165147
Derek Kastner <dkastner@gmail.com>
2017-02-27 22:31:47 -0600

    LOL no deleting plz

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :terms
-  resources :people
-  resources :governments
+  resources :terms, only: :show
+  resources :people, only: :show
+  resources :governments, only: :show
 
   get '/', to: 'pages#home'
 end
