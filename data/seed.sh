eed61c5a61b0394d683a6b3ce10d3770576dcdf0
Derek Kastner <dkastner@gmail.com>
2017-03-24 21:08:42 -0500

    Define people in import, not seed

--- data/seed.sh
+++ data/seed.sh
@@ -25,22 +25,6 @@ sql <<SQL
 UPDATE governments SET geom_webmercator = ST_Transform(geom, 4326);
 SQL
 
-
-# Add TX's two US senators
 sql <<SQL
-INSERT INTO people (slug, name, born, url, third_party_photo_url)
-('ted-cruz', 'Ted Cruz', '1970-12-22', 'https://www.cruz.senate.gov', 'https://upload.wikimedia.org/wikipedia/commons/8/87/Ted_Cruz%2C_official_portrait%2C_113th_Congress.jpg'),
-('john-cornyn', 'John Cornyn',)
-ON CONFLICT DO UPDATE;
+\\COPY people (slug,name,born,photo,third_party_photo_url,url) FROM people.csv WITH CSV HEADER
 SQL
-sql <<SQL
-INSERT INTO terms (slug, name, born, url, third_party_photo_url)
-('ted-cruz', 'Ted Cruz', '1970-12-22', 'https://www.cruz.senate.gov', 'https://upload.wikimedia.org/wikipedia/commons/8/87/Ted_Cruz%2C_official_portrait%2C_113th_Congress.jpg'),
-('john-cornyn', 'John Cornyn',)
-ON CONFLICT DO UPDATE;
-SQL
-
-
-# Download list of TX state senators
-
-wget "https://openstates.org/api/v1/legislators/?state=tx&chamber=upper"
